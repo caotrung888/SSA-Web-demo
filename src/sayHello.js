c73caf6c64442f88bc77407b76e78f9d2c56d1b8
trungcs <trungcs@teko.vn>
2021-09-30 16:00:53 +0700

    initial commit

---
+++ src/sayHello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hello() {
+    const sayHello = () => {
+        console.log('Hello');
+    }
+
+    return (
+        <div>
+            <button onClick={sayHello}>Say Hell Mai fen!</button>
+        </div>
+    );
+}
+
+export default Hello
